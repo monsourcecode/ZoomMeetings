4f8fe932eefc9a10a1dfbea3b3b7f574767d97a4
walid <01smartpk@gmail.com>
2020-06-02 19:00:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret and Redirect URL for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		clientID : '',
-		clientSecret : '',
+		clientID : 'a7IgUaZ_RlSIC6zGIRK7Gg',
+		clientSecret : '2A255iHNEEPLpuBkEVdpEJl5o3jnBvI1BpBe',
 		redirectUrl : ''
 	},
 	production:{	
-		clientID : '',
-		clientSecret : '',
+		clientID : 'a7IgUaZ_RlSIC6zGIRK7Gg',
+		clientSecret : '2A255iHNEEPLpuBkEVdpEJl5o3jnBvI1BpBe',
 		redirectUrl: ''
 	}
 };
